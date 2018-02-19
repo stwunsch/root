445d1477dcb25b28ba41200414983107a5fa18dd
Stefan Wunsch <stefan.wunsch@cern.ch>
2018-02-19 13:21:52 +0100

    a

--- build/test_dataloader.py
+++ build/test_dataloader.py
@@ -15,10 +15,8 @@ elif sys.argv[1] == "classic":
     f = ROOT.TFile("tmva_class_example.root")
     t = f.Get("TreeS")
     for i_event, event in enumerate(t):
-        x[i_event,0] = t.var1
-        x[i_event,1] = t.var2
-        x[i_event,2] = t.var3
-        x[i_event,3] = t.var4
+        for i_var, var in enumerate(["var1", "var2", "var3", "var4"]):
+            x[i_event,i_var] = getattr(event, var)
     f.Close()
 
 elif sys.argv[1] == "future":
