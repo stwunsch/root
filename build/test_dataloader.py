3a80c967a5a7dea3f1d30cab0ea12a17f93c60fa
Stefan Wunsch <stefan.wunsch@cern.ch>
2018-02-19 14:56:44 +0100

    a

--- build/test_dataloader.py
+++ build/test_dataloader.py
@@ -2,6 +2,7 @@ import numpy as np
 import ROOT
 import sys
 import argparse
+import root_numpy
 
 parser = argparse.ArgumentParser(description="Test TDFs as dataloader.")
 parser.add_argument("file", type=str, help="Test file")
@@ -17,33 +18,43 @@ print("Pool size: {}".format(ROOT.ROOT.GetImplicitMTPoolSize()))
 
 if args.mode == "classic":
     print("classic")
-    x = np.zeros((600000, 4), dtype=np.float32)
     f = ROOT.TFile(args.file)
     t = f.Get("TreeS")
+    x = np.zeros((t.GetEntries(), 4), dtype=np.float32)
     for i_event, event in enumerate(t):
         for i_var, var in enumerate(["var1", "var2", "var3", "var4"]):
             x[i_event, i_var] = getattr(event, var)
     f.Close()
 
-elif args.mode == "tdf":
-    print("tdf")
+elif args.mode == "tdf_ugly":
+    print("tdf_ugly")
     tdf = ROOT.Experimental.TDataFrame("TreeS", args.file)
     x = np.zeros((600000, 4), dtype=np.float32)
     for i_var, var in enumerate(["var1", "var2", "var3", "var4"]):
         for i_event, event in enumerate(tdf.TakeFloat(var)):
             x[i_event, i_var] = event
 
-elif args.mode == "tdf2":
-    print("tdf2")
+elif args.mode == "tdf":
+    print("tdf")
     tdf = ROOT.Experimental.TDataFrame("TreeS", args.file)
     x = np.array([
         np.fromiter(tdf.TakeFloat(v), np.float32)
         for v in ["var1", "var2", "var3", "var4"]
+    ])
+    x = x.T
+
+elif args.mode == "root_numpy":
+    print("root_numpy")
+    f = ROOT.TFile(args.file)
+    t = f.Get("TreeS")
+    x = np.array([
+        list(x) for x in root_numpy.tree2array(
+            t, branches=["var1", "var2", "var3", "var4"])
     ])
 
 elif args.mode == "future":
     tdf = ROOT.Experimental.TDataFrame("TreeS", args.file)
     x = tdf.Take(["var1", "var2", "var3", "var4"])
 
-#print(x.flags)
-#print(x)
+print(x.flags)
+print(x)
