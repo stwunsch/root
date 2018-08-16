69a49376f9b87f2cda8e1cfa13f7a2b85bcf2bf7
Stefan Wunsch <stefan.wunsch@cern.ch>
2018-08-16 13:14:11 +0200

    x

--- tutorials/vecops/vo005_Combinations.C
+++ tutorials/vecops/vo005_Combinations.C
@@ -1,15 +1,16 @@
 using namespace ROOT::VecOps;
 
-void vo005_Combinations(){
-    RVec<float> x = {1, 2, 3, 4};
-    cout << "Initial vector:\n  " << x << endl << endl;
+void vo005_Combinations()
+{
+   RVec<float> x = {1, 2, 3, 4};
+   RVec<float> y = {-1, -2, -3, -4};
+   cout << "Initial collections:\n  " << x << endl << y << endl;
 
-    auto c1 = Combinations(x, 3, 0);
-    auto c2 = Combinations(x, 3, 1);
-    auto c3 = Combinations(x, 3, 2);
-    auto z = c1 * c2 * c3;
-    cout << "Combinations:" << endl;
-    for(size_t i=0; i<z.size(); i++) {
-        cout << "  " << c1[i] << " * " << c2[i] << " * " << c3[i] << " = " << z[i] << endl;
-    }
+   auto c = Combinations(x, y);
+   auto z = Take(x, c[0]) * Take(y, c[1]);
+
+   cout << "Combinations:" << endl;
+   for (size_t i = 0; i < z.size(); i++) {
+      cout << "  " << x[c[0][i]] << " * " << y[c[1][i]] << " = " << z[i] << endl;
+   }
 }
